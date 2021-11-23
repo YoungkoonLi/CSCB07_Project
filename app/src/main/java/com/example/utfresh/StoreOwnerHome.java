3d388e5a299ec383dbd7aa8cc784f9e6951abbd2
Gisellew10 <94551327+Gisellew10@users.noreply.github.com>
2021-11-22 23:32:09 -0600

    Giselle: 1.Create AddNewProduct Activity 2. Create ProductList Activity 3. Design store_owner_home.xml 4.Write the function of product_list button 5. Write the function of add_new_products button

--- app/src/main/java/com/example/utfresh/StoreOwnerHome.java
+++ app/src/main/java/com/example/utfresh/StoreOwnerHome.java
@@ -2,7 +2,10 @@ package com.example.utfresh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StoreOwnerHome extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class StoreOwnerHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_owner_home);
+
+        //Function of product_list button
+        Button product_list = findViewById(R.id.product_list);
+        product_list.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                openActivityProductList();
+            }
+        });
+
+        //Function of add_new_products button
+        Button add_new = findViewById(R.id.add_new_products);
+        add_new.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                openActivityAddNewProduct();
+            }
+        });
+    }
+
+    public void openActivityProductList(){
+        Intent intent = new Intent(this, ProductList.class);
+        startActivity(intent);
+    }
+
+    public void openActivityAddNewProduct(){
+        Intent intent = new Intent(this, AddNewProduct.class );
+        startActivity(intent);
     }
 }
