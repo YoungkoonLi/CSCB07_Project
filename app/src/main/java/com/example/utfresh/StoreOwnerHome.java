6bd56f69d2d9001809bf7fb9bbbb3fde7e68475a
Gisellew10 <94551327+Gisellew10@users.noreply.github.com>
2021-11-22 21:30:38 -0600

    Giselle: add Store Owner Home Activity

---
+++ app/src/main/java/com/example/utfresh/StoreOwnerHome.java
@@ -0,0 +1,14 @@
+package com.example.utfresh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreOwnerHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_owner_home);
+    }
+}
