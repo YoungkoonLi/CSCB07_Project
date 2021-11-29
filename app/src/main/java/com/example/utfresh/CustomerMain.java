68d1c47b2957696573b9a7e7478dee8cae84cb99
YoungkoonLi <i.hate.you.for.loving.myself@gmail.com>
2021-11-29 03:41:39 -0500

    Added the basis of home page for customers (CustomerMain)

---
+++ app/src/main/java/com/example/utfresh/CustomerMain.java
@@ -0,0 +1,38 @@
+package com.example.utfresh;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class CustomerMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_main);
+    }
+
+    protected void loadStore(){
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("stores");
+        ref.child("s1").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("demo", "Error getting data", task.getException());
+                }
+                else {
+                    //load stores one by one.
+                }
+            }
+        });
+    }
+}
