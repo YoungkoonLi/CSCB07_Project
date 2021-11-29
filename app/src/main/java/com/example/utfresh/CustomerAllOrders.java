78c8a86e5b85bd4c5fc8ac6292155cc6f47265be
delighted5y <gpj5353@163.com>
2021-11-30 03:11:13 +0800

    Peijia Guo: (1)add the java file and the corresponding file for ppt page 6 of 16 (2) set strings "ready_for_pick_up", "pending", "complete" into file strings

---
+++ app/src/main/java/com/example/utfresh/CustomerAllOrders.java
@@ -0,0 +1,14 @@
+package com.example.utfresh;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CustomerAllOrders extends AppCompatActivity {
+    //this is for page 6 of 16 in the ppt file
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_all_orders);
+    }
+}
