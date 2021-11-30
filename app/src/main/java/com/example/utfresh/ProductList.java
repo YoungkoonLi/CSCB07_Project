ed50013b8aabaf595f2c259f84f1636550674e58
Gisellew10 <94551327+Gisellew10@users.noreply.github.com>
2021-11-29 23:21:04 -0600

    fix product_list errors

--- app/src/main/java/com/example/utfresh/ProductList.java
+++ app/src/main/java/com/example/utfresh/ProductList.java
@@ -37,26 +37,26 @@ public class ProductList extends AppCompatActivity {
         dao = new DAOData();
         loadData();
 
-        Button home = findViewById(R.id.home);
-        home.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                openActivityStoreOwnerHome();
-            }
-        });
-
-        Button orders = findViewById(R.id.orders);
-        orders.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                openActivityOrders();
-            }
-        });
-
-        Button setting = findViewById(R.id.setting);
-        setting.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                openActivitySetting();
-            }
-        });
+//        Button home = findViewById(R.id.home);
+//        home.setOnClickListener(new View.OnClickListener(){
+//            public void onClick(View v) {
+//                openActivityStoreOwnerHome();
+//            }
+//        });
+//
+//        Button orders = findViewById(R.id.orders);
+//        orders.setOnClickListener(new View.OnClickListener(){
+//            public void onClick(View v) {
+//                openActivityOrders();
+//            }
+//        });
+//
+//        Button setting = findViewById(R.id.setting);
+//        setting.setOnClickListener(new View.OnClickListener(){
+//            public void onClick(View v) {
+//                openActivitySetting();
+//            }
+//        });
     }
 
     private void loadData() {
@@ -80,21 +80,21 @@ public class ProductList extends AppCompatActivity {
         });
     }
 
-    public void openActivityStoreOwnerHome(){
-        Intent intent = new Intent(this, StoreOwnerHome.class);
-        startActivity(intent);
-    }
-
-
-    public void openActivityOrders(){
-        Intent intent = new Intent(this, Orders.class);
-        startActivity(intent);
-    }
-
-    public void openActivitySetting(){
-        Intent intent = new Intent(this, Setting.class);
-        startActivity(intent);
-    }
+//    public void openActivityStoreOwnerHome(){
+//        Intent intent = new Intent(this, StoreOwnerHome.class);
+//        startActivity(intent);
+//    }
+//
+//
+//    public void openActivityOrders(){
+//        Intent intent = new Intent(this, Orders.class);
+//        startActivity(intent);
+//    }
+//
+//    public void openActivitySetting(){
+//        Intent intent = new Intent(this, Setting.class);
+//        startActivity(intent);
+//    }
 
 
 }
