359d6ba14d677c6be626b8310e1a4262f7a85ade
rann8743 <lran8743@gamil.com>
2021-12-01 08:13:58 +0800

    Ran: correct SignIn page

--- app/src/main/java/com/example/utfresh/MainActivity.java
+++ app/src/main/java/com/example/utfresh/MainActivity.java
@@ -1,101 +1,177 @@
 package com.example.utfresh;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    RadioButton store;
-    RadioButton cus;
-    RadioGroup radiogroup;
-    Button signupForStore;
-    Button signupForCustomer;
-    Button cusButton, storeButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editEmail,editPassword;
+    RadioGroup radioGroup;
+    RadioButton store, customer;
+    Button login, CusSignUp, StoreSignUp;
+    CheckBox remember;
+    FirebaseAuth fAuth;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        store=findViewById(R.id.radioStoreOwner);
-        cus=findViewById(R.id.radioCustomer);
-        radiogroup=findViewById(R.id.radioGroup);
-        cusButton=findViewById(R.id.buttonForCus);
-        storeButton=findViewById(R.id.buttonForStore);
+        //initial
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
+        radioGroup = findViewById(R.id.radioGroup);
+        store = findViewById(R.id.radioStoreOwner);
+        customer = findViewById(R.id.radioCustomer);
+        login = findViewById(R.id.login);
+        CusSignUp = findViewById(R.id.signupForCustomer);
+        StoreSignUp = findViewById(R.id.signupForStore);
+        remember = findViewById(R.id.checkBox);
+
+        preferences = getSharedPreferences("bo7", Context.MODE_PRIVATE);
+        editor = preferences.edit();
+        fAuth = FirebaseAuth.getInstance();
+        Remember();
+
+        //initial button for CusSignUp
+        CusSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CustomerSignUp(v);
+            }
+        });
+        //initial button for StoreSignUp
+        StoreSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StoreSignUp(v);
+            }
+        });
         //radioGroup
-        radiogroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton radioButton = (RadioButton) findViewById(checkedId);
-                Toast.makeText(getApplicationContext(), "Login as " + radioButton.getText(), Toast.LENGTH_LONG).show();
+                RadioButton radioButton = findViewById(checkedId);
+                Toast.makeText(getApplicationContext(), "Login As " + radioButton.getText(), Toast.LENGTH_LONG).show();
             }
         });
-
-        //button for signUpForStore
-        signupForStore = findViewById(R.id.signupForStore);
-        signupForStore.setOnClickListener(new View.OnClickListener() {
+        //initial button for login
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayUser2();
-            }
-        });
-        //button for signUpForCustomer
-        signupForCustomer = findViewById(R.id.signupForCustomer);
-        signupForCustomer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayUser3();
-            }
-        });
-        cusButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayUser4();
-            }
-        });
-        storeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayUser5();
+                setLogin(v);
             }
         });
     }
 
-//    public void displayUser(View view){
-////        if(customer){
-////            Intent intent = new Intent(MainActivity.this, CustomerLogin.class);
-////            intent.putExtra();
-////            startActivity(intent);
-////        }
-//        if(storeowner){
-//            Intent intent = new Intent(MainActivity.this, StoreOwnerHome.class);
-//            intent.putExtra();
-//            startActivity(intent);
-//        }
-//    }
-
-    public void displayUser2(){
-        Intent intent = new Intent(this, StoreSignUp.class);
-        startActivity(intent);
+    public void Remember(){
+        boolean re = preferences.getBoolean("remember", false);
+        String email = preferences.getString("email", "");
+        String pass = preferences.getString("password", "");
+        editEmail.setText(email);
+        editPassword.setText(pass);
+        remember.setChecked(re);
     }
 
-    public void displayUser3(){
-        Intent intent = new Intent(MainActivity.this, CustomerSignUp.class);
-        startActivity(intent);
+    public void CustomerSignUp(View view){
+//        Intent intent = new Intent(MainActivity.this,CustomerSignUp.class);
+//        startActivity(intent);
+        startActivity(new Intent(MainActivity.this, CustomerSignUp.class));
     }
-    public void displayUser4(){
-        Intent intent = new Intent(this, CustomerMain.class);
-        startActivity(intent);
+    public void StoreSignUp(View view){
+        startActivity(new Intent(MainActivity.this, StoreSignUp.class));
     }
-    public void displayUser5(){
-        Intent intent = new Intent(this, StoreOwnerHome.class);
-        startActivity(intent);
+    public void setLogin(View view){
+        //initial input string
+        String email = editEmail.getText().toString().trim();
+        String pass = editPassword.getText().toString().trim();
+        //check remember me
+        boolean checked = remember.isChecked();
+        editor.putBoolean("remember", checked);
+        if(checked){
+            editor.putString("email", email);
+            editor.putString("password", pass);
+        }else{
+            editor.putString("email", "");
+            editor.putString("password", "");
+        }
+        editor.apply();
+
+        //email and password can not be empty
+        if(TextUtils.isEmpty(email)){
+            editEmail.setError("email is required!");
+            return;
+        }
+        if(TextUtils.isEmpty(pass)){
+            editPassword.setError("password is required");
+            return;
+        }
+        //set restriction to email and password
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editEmail.setError("Please provide valid email!");
+            return;
+        }
+        if(pass.length() < 6){
+            editPassword.setError("Password should have at least 6 characters!");
+        }
+        //
+
+        boolean isStore = store.isChecked();
+        boolean isCus = customer.isChecked();
+
+        if(!isCus && !isStore){
+            Toast.makeText(getApplicationContext(), "Please select login as Customer or StoreOwner!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        fAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+//                    Toast.makeText(getApplicationContext(), "Logged successfully!", Toast.LENGTH_LONG).show();
+//                    startActivity(new Intent(MainActivity.this, StoreHome.class));
+
+                    if(isCus){
+                        Toast.makeText(getApplicationContext(), "Logged successfully!", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(MainActivity.this, CustomerMain.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Logged successfully!", Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(MainActivity.this, StoreOwnerHome.class));
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(), "Error! " + task.getException(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
+
 }
