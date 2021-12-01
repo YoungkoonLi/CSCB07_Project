211c418386969e9bfdc812934a3d9ddd033af790
Gisellew10 <94551327+Gisellew10@users.noreply.github.com>
2021-12-01 15:54:25 -0600

    Fix errors in add new product

--- app/src/main/java/com/example/utfresh/DAOData.java
+++ app/src/main/java/com/example/utfresh/DAOData.java
@@ -1,6 +1,8 @@
 package com.example.utfresh;
 
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
@@ -11,18 +13,34 @@ public class DAOData {
 
     // this class is to use the firebase database: use the add method to insert product into the firebase
 
-
     private DatabaseReference databaseReference;
-    public DAOData(){
-        FirebaseDatabase db = FirebaseDatabase.getInstance();
-        databaseReference = db.getReference(Data.class.getSimpleName());
+    FirebaseUser user;
+    String uid;
 
+
+    public DAOData(){
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        uid = user.getUid();
+        databaseReference = FirebaseDatabase.getInstance().getReference(Data.class.getSimpleName());
 
     }
     public Task<Void> add(Data data)
     {
-        return databaseReference.push().setValue(data);
+        return databaseReference.child(uid).push().setValue(data);
     }
+
+
+//    private DatabaseReference databaseReference;
+//    public DAOData(){
+//        FirebaseDatabase db = FirebaseDatabase.getInstance();
+//        databaseReference = db.getReference(Data.class.getSimpleName());
+//
+//
+//    }
+//    public Task<Void> add(Data data)
+//    {
+//        return databaseReference.push().setValue(data);
+//    }
 
 //    public Task<Void> update(String key, HashMap<String, Object> hashMap){
 //        return databaseReference.child(key).updateChildren(hashMap);
