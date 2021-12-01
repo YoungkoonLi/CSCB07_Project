9110b45cf28eb9162813ffd7c3ba491fee7be213
delighted5y <gpj5353@163.com>
2021-12-01 15:18:00 +0800

    Peijia Guo: fix bugs for the day/night version in CustomerAllOrders

--- app/src/main/java/com/example/utfresh/CustomerAllOrders.java
+++ app/src/main/java/com/example/utfresh/CustomerAllOrders.java
@@ -6,9 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class CustomerAllOrders extends AppCompatActivity {
     //this is for page 6 of 16 in the ppt file
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_all_orders);
+
     }
 }
