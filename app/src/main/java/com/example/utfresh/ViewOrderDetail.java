d516e891a657840ea074e7a7df7b301e128853d0
Gisellew10 <94551327+Gisellew10@users.noreply.github.com>
2021-12-01 23:51:46 -0600

    Continue to work on View Order Detail

--- app/src/main/java/com/example/utfresh/ViewOrderDetail.java
+++ app/src/main/java/com/example/utfresh/ViewOrderDetail.java
@@ -1,5 +1,6 @@
 package com.example.utfresh;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,12 +10,27 @@ import android.os.Bundle;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class ViewOrderDetail extends AppCompatActivity {
 
     TextView username;
     Spinner spinner_order_status;
     SwipeRefreshLayout swipeRefreshLayout;
     RecyclerView recyclerView;
+    DatabaseReference databaseReference;
+    DatabaseReference store;
+    FirebaseUser user;
+    String uid;
+    OrderDetailAdapter adapter;
 
 
     @Override
@@ -22,13 +38,44 @@ public class ViewOrderDetail extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_order_detail);
 
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference("Order");
+        uid = user.getUid();
+        store = databaseReference.child(uid);
+
         username = (TextView) findViewById(R.id.username);
         spinner_order_status = (Spinner) findViewById(R.id.spinner_order_status);
         swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swip);
         recyclerView = (RecyclerView) findViewById(R.id.Item_List);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
+        adapter = new OrderDetailAdapter(this);
+        recyclerView.setAdapter(adapter);
+        loadData();
 
 
+
+    }
+
+    private void loadData() {
+        store.child("Item_List").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ArrayList<OrderData> data_set = new ArrayList<>();
+                for(DataSnapshot value : snapshot.getChildren()){
+                    OrderData data = value.getValue(OrderData.class);
+                    data_set.add(data);
+                }
+                adapter.setItems(data_set);
+                adapter.notifyDataSetChanged();
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
