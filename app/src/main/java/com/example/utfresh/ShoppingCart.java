fc55e32b42d96a713f426b2d856b843c7429b6eb
delighted5y <gpj5353@163.com>
2021-12-03 18:25:42 +0800

    Peijia Guo: build java and xml file for shopping cart and link that to customer_all_orders

---
+++ app/src/main/java/com/example/utfresh/ShoppingCart.java
@@ -0,0 +1,29 @@
+package com.example.utfresh;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShoppingCart extends AppCompatActivity {
+
+    Button CheckOut;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
+
+        CheckOut = findViewById(R.id.Checkout);
+        CheckOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // redirect activity to activity_customer_all_orders
+                Intent intent = new Intent(ShoppingCart.this, CustomerAllOrders.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
