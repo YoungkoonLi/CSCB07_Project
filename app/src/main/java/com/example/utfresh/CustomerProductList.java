f377687050f55ba9ef1c68bfdf3db2e58fc6cfbb
YoungkoonLi <i.hate.you.for.loving.myself@gmail.com>
2021-12-04 02:08:44 -0500

    Yangkun: Added one line of comment to CustomerProductList.java

--- app/src/main/java/com/example/utfresh/CustomerProductList.java
+++ app/src/main/java/com/example/utfresh/CustomerProductList.java
@@ -11,7 +11,7 @@ public class CustomerProductList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_product_list);
-
+        //Initialized recyclerview here.
     }
 
 
