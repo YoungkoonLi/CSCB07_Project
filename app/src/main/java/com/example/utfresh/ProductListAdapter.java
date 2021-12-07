01fe47ceadc053ec27f94528e98d0702a4772a9f
YoungkoonLi <i.hate.you.for.loving.myself@gmail.com>
2021-12-07 15:54:09 -0500

    Yangkun: Added and fixed add to cart function of customer product list page.

--- app/src/main/java/com/example/utfresh/ProductListAdapter.java
+++ app/src/main/java/com/example/utfresh/ProductListAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,8 +25,9 @@ public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.
     ArrayList<String> names;
     ArrayList<String> prices;
     ArrayList<String> categories;
-    static int[] quantities;
-    public ProductListAdapter(Context context){
+    ArrayList<Integer> quantities;
+
+    public ProductListAdapter(Context context) {
         this.context = context;
         //Initialize to empty list. Awaiting data from firebase.
         this.names = new ArrayList<>();
@@ -45,12 +47,16 @@ public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.
         holder.name.setText(names.get(position));
         holder.price.setText("$" + prices.get(position));
         holder.category.setText(categories.get(position));
-
+        holder.add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addToCart(holder.name.getText().toString(), holder.quantity.getText().toString());
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        Log.e("text", names.size() + "");
         return names.size();
     }
 
@@ -58,33 +64,45 @@ public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.
      * This is not done in the constructor because data must be retrieved from firebase before
      * populating the lists.
      */
-    public void setAllList(ArrayList<String> names, ArrayList<String> prices, ArrayList<String> categories){
+    public void setAllList(ArrayList<String> names, ArrayList<String> prices, ArrayList<String> categories) {
         this.names.addAll(names);
         this.prices.addAll(prices);
         this.categories.addAll(categories);
-        this.quantities = new int[names.size()];
+        this.quantities = new ArrayList<>();
         //zero out the list
-        for(int i = 0; i < quantities.length; i++){
-            quantities[i] = 0;
+        for (int i = 0; i < names.size(); i++) {
+            quantities.add(0);
         }
         notifyDataSetChanged();
     }
 
-    public void addToCart(String name){
-        int index = names.indexOf(name);
-        quantities[index]++;
-        Toast.makeText(context, "Added 1 " + name +"to shopping cart", Toast.LENGTH_SHORT);
+    public void addToCart(String name, String quantity) {
+        if (quantity.matches("\\d+") && Integer.valueOf(quantity) > 0) {
+            int index = names.indexOf(name);
+            if (index >= 0) {
+                quantities.set(index, quantities.get(index) + Integer.valueOf(quantity));
+                Toast.makeText(context, "Added " + Integer.valueOf(quantity) + name + " to shopping cart", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "No such product!", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(context, "Invalid quantity!", Toast.LENGTH_SHORT).show();
+        }
     }
-    public class ProductViewHolder extends RecyclerView.ViewHolder{
+
+    public class ProductViewHolder extends RecyclerView.ViewHolder {
         TextView name;
         TextView price;
         TextView category;
+        EditText quantity;
         Button add;
+
         public ProductViewHolder(@NonNull View itemView) {
             super(itemView);
             name = itemView.findViewById(R.id.product_name);
             price = itemView.findViewById(R.id.product_price);
             category = itemView.findViewById(R.id.product_category);
+            quantity = itemView.findViewById(R.id.product_quantity);
             add = itemView.findViewById(R.id.add_cart);
         }
     }
